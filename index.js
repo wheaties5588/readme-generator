46579d44f82ea318c11c7733d8e2125ada1fa657
Matt Wieciech <matt.wieciech@gmail.com>
2020-07-13 21:05:36 -0400

    Created combined array to string to write to file

--- index.js
+++ index.js
@@ -5,8 +5,13 @@ inquirer
     .prompt([
         {
             type: "input",
-            message: "What is your name?",
-            name: "name"
+            message: "What is the title of your project?",
+            name: "title"
+        },
+        {
+            type: "input",
+            message: "Enter a description for your project:",
+            name: "description"
         },
         {
             type: "checkbox",
@@ -18,15 +23,36 @@ inquirer
     .then((res) => {
         console.log(res);
         console.log("Creating README file...");
-        let resJSON = JSON.stringify(res);
-        fs.writeFile("README-test.md", resJSON, (err) => {
-            if (err) {
-                throw err;
-            } else {
-                console.log("README file successfully created!");
-            }
-        })
+        createREADMEFile(res);
+        
     })
     .catch((err) => {
         console.log(err);
     })
+    
+    
+function createREADMEFile(input) {
+    
+    // let inputJSON = JSON.stringify(input);
+    
+    let readmeTitle = `# ${input.title}`;
+    
+    let readmeDescription = `${input.description}`;
+    
+    // console.log(readmeDescription);
+      
+    let completeREADME = readmeTitle + readmeDescription;
+    
+    const entries = Object.entries(input).join('  \n');
+
+  
+    console.log(JSON.stringify(entries));
+        
+    fs.writeFile("README-test.md", entries, (err) => {
+        if (err) {
+            throw err;
+        } else {
+            console.log("README file successfully created!");
+        }
+    })
+}
