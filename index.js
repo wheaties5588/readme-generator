5797a7c578acf7763061a8d088d3e433b1b25331
Matt Wieciech <matt.wieciech@gmail.com>
2020-07-15 19:52:28 -0400

    More questions with setup to print to file, logic to create numbered list

--- index.js
+++ index.js
@@ -14,14 +14,32 @@ inquirer
             name: "description"
         },
         {
-            type: "checkbox",
-            message: "What languages did you use?",
-            name: "languages",
-            choices: [ "HTML", "CSS", "Javascript", "Jquery", "Node.js", "React", "SQL" ]
+            type: "input",
+            message: "Enter installation insructions as a comma separated list:",
+            name: "install"
+        },
+        {
+            type: "input",
+            message: "Enter usage information for your project:",
+            name: "usage"
+        },
+        {
+            type: "list",
+            message: "Which license does this project fall under?",
+            name: "license",
+            choices: [
+                "MIT License",
+                "Code Project Open License (CPOL)",
+                "Common Development and Distribution License (CDDL)",
+                "Microsoft Public License (Ms-PL)",
+                "Mozilla Public License 1.1 (MPL 1.1)",
+                "Common Public License Version 1.0 (CPL)",
+                "Eclipse Public License 1.0",
+                "Apache License, Version 2.0"
+            ]
         },
     ])
     .then((res) => {
-        console.log(res);
         console.log("Creating README file...");
         createREADMEFile(res);
         
@@ -32,23 +50,70 @@ inquirer
     
     
 function createREADMEFile(input) {
+    let readmeTitle;
+    let readmeDescription;
+    const descriptionHead = "## Description";
+    let tableOfContents;
+    const tocHead = "## Table of Contents";
+    let installArr;
+    const installHead = "## Installation";
+    let readmeUsage;
+    const usageHead = "## Usage";
     
-    // let inputJSON = JSON.stringify(input);
+    let completeREADME = [];
     
-    let readmeTitle = `# ${input.title}`;
+    // Adding Title
+    if (input.title == '') {
+        readmeTitle = '# TITLE';
+    } else {
+        readmeTitle = `# ${input.title}`;
+    }
+    completeREADME.push(readmeTitle);
     
-    let readmeDescription = `${input.description}`;
     
-    // console.log(readmeDescription);
-      
-    let completeREADME = readmeTitle + readmeDescription;
+    // Adding description
+    if (input.description == '') {
+        readmeDescription = `${descriptionHead}\n Enter project description here.`;
+    } else {
+        readmeDescription = `${descriptionHead}\n${input.description}`;
+    }
+    completeREADME.push(readmeDescription);
     
-    const entries = Object.entries(input).join('  \n');
+    
+    //Adding Table of Contents
+    tableOfContents = `${tocHead}\n* [Installation](#installation)\n* [Usage](#usage)\n* [Credits](#credits)\n* [License](#license)\n`;
+    completeREADME.push(tableOfContents);
+    
+    
+    //Adding installation instructions
+    completeREADME.push(`${installHead}`);
+    
+    installArr = input.install.split(',').map(item => {
+        return `${item.trim()}`;
+    });
+    
+    for (var i = 0; i < installArr.length; i++) {
+        completeREADME.push(`${i + 1}. ${installArr[i]}`);
+    }
+    
+    
+    //Adding Usage
+    if (input.usage == '') {
+        readmeUsage = `\n${usageHead}\n Enter project usage here.`;
+    } else {
+        readmeUsage = `\n${usageHead}\n${input.usage}`;
+    }
+    completeREADME.push(readmeUsage);
 
-  
-    console.log(JSON.stringify(entries));
+
+     
+    
+    //Joining the created README Array with a newline separator
+    const README = completeREADME.join('\n');
         
-    fs.writeFile("README-test.md", entries, (err) => {
+    
+    //Creating the README
+    fs.writeFile("README-test.md", README, (err) => {
         if (err) {
             throw err;
         } else {
